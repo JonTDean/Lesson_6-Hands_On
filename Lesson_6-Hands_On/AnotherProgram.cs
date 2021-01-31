78b0c3cc04f074762089e36910e84704dd05d476
JonTDean <JonathanTomDean@Gmail.com>
2021-01-31 06:22:53 -0500

    This lesson was horrible

---
+++ Lesson_6-Hands_On/AnotherProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+/* [ x ] 1. Create another file named AnotherProgram.cs.
+ * [ x ] 2. Within AnotherProgram.cs, include the following: 
+
+        [ x ] - The necessary using statement for System
+                   vvv THIS IS BAD PRACTICE, FIRE WHOEVER WROTE THIS QUESTION vvv
+        [ x ] - A namespace named myNamespace. Within this namespace add the following: 
+           [ x ] - A class named AnotherProgram
+           [ x ] - Add a method within the class named printText that prints "Hello Earth!" to the console.
+
+ * [ x ] 3. Again within the AnotherProgram.cs file, create another namespace. 
+        [ x ] - Give this namespace a unique name.
+        [ x ] - The namespace should have a class that includes a method. 
+            [ x ] - The method should have a unique print statement.
+
+*/
+
+
+// I'm going to just follow the directions at this point
+namespace myNamespace // https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-1.1/893ke618(v=vs.71)?redirectedfrom=MSDN
+{
+    public class AnotherProgram     // WHY TORTURE US WITH NONSTATICS
+    {
+        public void printText()
+        {
+            Console.WriteLine("Hello Earth!");
+        }
+    }
+}
+
+// Who wrote this question? Tell them to hand in their two week notice.
+namespace Unique
+{
+    public class TheClass           // WHY TORTURE US WITH NONSTATICS 
+    {
+        public void theMethod()
+        {
+            Console.WriteLine("Most unique print statement.");
+        }
+    }
+}
--- Lesson_6-Hands_On/Program.cs
+++ Lesson_6-Hands_On/Program.cs
@@ -1,4 +1,20 @@
 ﻿using System;
+using myNamespace;
+using Unique;
+
+/* [ x ] 4. Using the using keyword, make both of the
+ *    namespaces from AnotherProgram.cs available 
+ *    to the Program.cs file using aliases.
+ 
+ * [ x ] 5. Within the Main method in Program.cs, call each
+ *    of the methods from the namespaces in AnotherProgram.cs.
+ 
+ * [ x ] 6. Your final output should look similar to the following: 
+ 
+        Hello World!
+        Hello Earth!
+        I am text from the textNamespace. 
+*/
 
 namespace Lesson_6_Hands_On
 {
@@ -6,7 +22,15 @@ namespace Lesson_6_Hands_On
     {
         static void Main(string[] args)
         {
+            // WHY TORTURE US WITH NONSTATICS 
+            AnotherProgram ap = new AnotherProgram();
+            
+            // WHY TORTURE US WITH NONSTATICS 
+            TheClass tc = new TheClass();
+
             Console.WriteLine("Hello World!");
+            ap.printText();
+            tc.theMethod();
         }
     }
 }
