1be833b26f0567b99f8e04bbf8c74de3ab1825c9
JonTDean <JonathanTomDean@Gmail.com>
2021-01-31 04:05:44 -0500

    Beginning

---
+++ Lesson_6-Hands_On/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_6_Hands_On
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
